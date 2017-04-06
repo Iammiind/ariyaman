13022f42ce7a9cc8014973f34bee9371b2c9b88b
Iammmiind <Onanong.pureemahawong@gmail.com>
2017-04-06 19:11:13 +0700

    Finish issues GH-06 GH-07 GH-08

--- farmapp/src/View.java
+++ farmapp/src/View.java
@@ -3,16 +3,7 @@ import ariyamanTeam.farmapp.controller.Controller;
 public class View {
 
 	public static void main(String[] args) {
-		Controller test = new Controller();
-		test.plantSeed(1);
-		test.plantSeed(2);
-		test.plantSeed(3);
-		test.viewAllPlants();
-		test.sleep1day();
-		test.sleep1day();
-		test.sleep1day();
-		test.sleep1day();
-		test.viewAllPlants();
+		
 	}
 
 }
